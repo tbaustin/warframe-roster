7e35a021a1327f015eea4a0fabab4371da55e654
Kennedy Rose <kennedy@kennedyrose.com>
2017-10-16 16:05:18 -0500

    defer zygote script

--- components/zygote.js
+++ components/zygote.js
@@ -6,7 +6,7 @@ export default class extends React.Component {
 		return (
 			<div>
 				<link type='text/css' rel='stylesheet' href='https://zygote.netlify.com/zygote-v1.css' />
-				<script src='https://zygote.netlify.com/zygote-v1.js' />
+				<script src='https://zygote.netlify.com/zygote-v1.js' defer />
 			</div>
 		)
 	}
