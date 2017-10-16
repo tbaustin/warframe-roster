9dc929bb0822aca18eb4ff524683f6fc007e8a16
Kennedy Rose <kennedy@kennedyrose.com>
2017-10-16 10:54:20 -0500

    Update dependencies

--- utils/update.js
+++ utils/update.js
@@ -2,6 +2,7 @@
 const exec = require('child-process-promise').exec
 const fs = require('fs-extra')
 const cpy = require('cpy')
+const curPkg = require('package.json')
 
 console.log('Updating boilerplate...')
 fs.remove('temp-update')
@@ -28,8 +29,34 @@ fs.remove('temp-update')
 		})
 	})
 	.then(() => {
+		console.log('Adding new dependencies...')
+		const newPkg = require('./temp-update/package.json')
+		let newDep = {}
+		let newDevDep = {}
+		for(let i in newPkg.dependencies){
+			if(!(i in curPkg.dependencies) || curPkg.dependencies[i] !== newPkg.dependencies[i]){
+				newDep[i] = newPkg.dependencies[i]
+			}
+		}
+		for(let i in newPkg.devDependencies){
+			if(!(i in curPkg.devDependencies) || curPkg.devDependencies[i] !== newPkg.devDependencies[i]){
+				newDevDep[i] = newPkg.devDependencies[i]
+			}
+		}
+		let promises = []
+		for(let i in newDep){
+			promises.push(exec(`yarn add ${i}@${newDep[i]}`))
+		}
+		for(let i in newDevDep){
+			promises.push(exec(`yarn add ${i}@${newDevDep[i]}`))
+		}
+		return Promise.all(promises)
+	})
+	.then(() => {
 		console.log('Removing temporary files...')
-		//return fs.remove('temp-update')
+		return Promise.all([
+			fs.remove('temp-update')
+		])
 	})
 	.then(() => console.log('Done updating boilerplate.'))
 	.catch(err => { throw err })
