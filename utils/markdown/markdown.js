75f7644a95cb05ef22007e10424c0366609011f2
Kennedy Rose <kennedy@kennedyrose.com>
2017-11-04 10:48:03 -0500

    Check for markdown pages path

--- utils/markdown/markdown.js
+++ utils/markdown/markdown.js
@@ -2,20 +2,28 @@
 const Metalsmith = require('metalsmith')
 const markdown = require('metalsmith-markdown')
 const json = require('metalsmith-to-json')
+const fs = require('fs-extra')
 
 module.exports = (cwd, src, out) => {
 	console.log('Outputting markdown to JSON...')
 	return new Promise((resolve, reject) => {
-		Metalsmith('./')
-			.clean(false)
-			.source('markdown/pages')
-			.destination('./json/markdown/pages')
-			.use(markdown())
-			.use(json({ outputPath: '' }))
-			.build(err => {
-				if (err) return reject(err)
-				console.log('Output markdown to JSON.')
-				resolve()
+		fs.pathExists('markdown/pages')
+			.then(exists => {
+				if(!exists){
+					console.log('No markdown pages found')
+					return resolve()
+				}
+				Metalsmith('./')
+					.clean(false)
+					.source('markdown/pages')
+					.destination('./json/markdown/pages')
+					.use(markdown())
+					.use(json({ outputPath: '' }))
+					.build(err => {
+						if (err) return reject(err)
+						console.log('Output markdown to JSON.')
+						resolve()
+					})
 			})
 	})
 }
