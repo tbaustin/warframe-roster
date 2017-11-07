3c030c8d7e52add1ed4b7156d7c8232756d3270a
Kennedy Rose <kennedy@kennedyrose.com>
2017-11-07 09:31:31 -0600

    Warnings on environment APIs

--- utils/product/get-price.js
+++ utils/product/get-price.js
@@ -4,15 +4,20 @@ const env = require('../../json/env.json')
 const ids = require('../../json/product-ids.json')
 
 module.exports = () => {
-	return fetch(env.PRICING_API, {
-			method: 'POST',
-			body: JSON.stringify({
-				site: env.ECOMMERCE_API_SITE || 'all',
-				ids: ids
+	if (env.PRICING_API) {
+		return fetch(env.PRICING_API, {
+				method: 'POST',
+				body: JSON.stringify({
+					site: env.ECOMMERCE_API_SITE || 'all',
+					ids: ids
+				})
 			})
-		})
-		.then(res => res.json())
-		.then(res => {
-			return res
-		})
+			.then(res => res.json())
+	}
+	console.log('Warning: PRICING_API variable not found in environment')
+	let obj = {}
+	ids.map(id => {
+		obj[id] = 0
+	})
+	return Promise.resolve(obj)
 }
--- utils/product/get-stock.js
+++ utils/product/get-stock.js
@@ -4,16 +4,20 @@ const env = require('../../json/env.json')
 const ids = require('../../json/product-ids.json')
 
 module.exports = () => {
-	return fetch(env.STOCK_API, {
-			method: 'POST',
-			body: JSON.stringify({
-				site: env.ECOMMERCE_API_SITE || 'all',
-				ids: ids
+	if (env.STOCK_API) {
+		return fetch(env.STOCK_API, {
+				method: 'POST',
+				body: JSON.stringify({
+					site: env.ECOMMERCE_API_SITE || 'all',
+					ids: ids
+				})
 			})
-		})
-		.then(res => res.json())
-		.then(stock => {
-			console.log(stock)
-			return stock
-		})
+			.then(res => res.json())
+	}
+	console.log('Warning: STOCK_API variable not found in environment')
+	let obj = {}
+	ids.map(id => {
+		obj[id] = 0
+	})
+	return Promise.resolve(obj)
 }
