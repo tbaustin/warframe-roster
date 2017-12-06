603f80232e474071edf42a6d50b3411983c23ff2
Kennedy Rose <kennedy@kennedyrose.com>
2017-12-06 09:01:07 -0600

    Image file name

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,12 @@ import Img from 'gatsby-image'
 export default class Index extends React.Component {
 	render() {
 		console.log(this.props.data)
+		const img = this.props.data.testFile.childImageSharp
 		return (
 			<section>
 				<div>Image:</div>
 				<div className='container'>
-					<Img sizes={this.props.data.file.childImageSharp.sizes} />
+					<Img sizes={img.sizes} />
 				</div>
 				<style jsx>{`
 					.container{
@@ -21,14 +22,14 @@ export default class Index extends React.Component {
 }
 
 export const pageQuery = graphql`
-  query GatsbyImageSampleQuery {
-    file(relativePath: { eq: "test.jpg" }) {
-      childImageSharp {
-			sizes(maxWidth: 300) {
-				...GatsbyImageSharpSizes_noBase64
+	query GatsbyImageSampleQuery {
+		testFile: file(relativePath: { eq: "test.jpg" }) {
+			childImageSharp {
+				sizes(maxWidth: 300) {
+					...GatsbyImageSharpSizes_noBase64
+				}
 			}
-      }
-    }
-  }
+		}
+	}
 `
 
