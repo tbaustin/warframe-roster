9060c7d160610a75fcd4c6ee20cdeec34bc4d762
Kennedy Rose <kennedy@kennedyrose.com>
2017-12-14 10:37:31 -0600

    Close favicon tag

--- src/html.js
+++ src/html.js
@@ -19,7 +19,7 @@ class Html extends Component {
 					<meta charSet="utf-8" />
 					<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 					<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-					<link rel="icon" type="image/x-icon" href="/favicon.ico">
+					<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 				</head>
 				<body>
 					<div
