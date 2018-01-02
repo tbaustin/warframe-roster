9b946187e8ceebe5884a111aa92d8b18c2bdee52
Kennedy Rose <kennedy@kennedyrose.com>
2018-01-02 14:24:51 -0600

    Removed Salsify

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,56 +7,8 @@ import { OpenCart, AddToCart, CartQty, CartHasQty } from 'react-snipcart'
 
 export default class Index extends React.Component {
 	render() {
-		const img = this.props.data.testFile
 		return (
-			<div>
-				{/*
-				<MobXExample />
-				*/}
-				<AddToCart data={{
-					id: 'ABC123',
-					name: 'Test Product',
-					url: '/',
-					price: '499.99',
-					openCart: true,
-				}}>
-					<div>Add to Cart</div>
-				</AddToCart>
-				<OpenCart>
-					<CartQty />
-				</OpenCart>
-				<div>Image:</div>
-				<div className='container'>
-					<Img sizes={img.sizes} />
-				</div>
-				<style jsx>{`
-					.container{
-						width: 300px;
-					}
-				`}</style>
-			</div>
+			<div>Home page!</div>
 		)
 	}
 }
-
-
-// MSRP
-export const pageQuery = graphql`
-	query HomepageQueries {
-		productData: allMarkdownRemark {
-			edges {
-				node {
-					html
-					frontmatter {
-						title
-					}
-				}
-			}
-		}
-		testFile: imageSharp(id: { regex: "/test.jpg/" }) {
-			sizes(maxWidth: 300) {
-				...GatsbyImageSharpSizes_noBase64
-			}
-		}
-	}
-`
