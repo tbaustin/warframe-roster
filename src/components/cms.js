f90654d8c36b3eeeaf6b0e43992e7f44cfc5f3f9
Kennedy Rose <kennedyianrose@gmail.com>
2018-07-27 16:46:50 -0500

    Added CMS global styles

--- src/components/cms.js
+++ src/components/cms.js
@@ -7,6 +7,21 @@ injectGlobal({
 	'.nc-githubAuthenticationPage-logo svg': {
 		display: `none !important`,
 	},
+	'.nc-githubAuthenticationPage-logo': {
+		background: `url('../img/logo.png') no-repeat center center !important`,
+		backgroundSize: `contain !important`,
+	},
+	'.nc-githubAuthenticationPage-button': {
+		color: `transparent !important`,
+	},
+	'.nc-githubAuthenticationPage-button::after': {
+		content: `Sign In`,
+		textAlign: `center`,
+		color: `#fff`,
+		position: `absolute`,
+		left: 0,
+		right: 0,
+	},
 })
 
 window.netlifyIdentity = identity
