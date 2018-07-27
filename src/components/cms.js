188e89efaabc9d8cc41d9c9fc34fb7f18a5d2a9e
Kennedy Rose <kennedyianrose@gmail.com>
2018-07-27 16:42:56 -0500

    Removed cms css import

--- src/components/cms.js
+++ src/components/cms.js
@@ -2,7 +2,6 @@ import CMS from 'netlify-cms'
 import 'netlify-cms/dist/cms.css'
 import identity from 'netlify-identity-widget'
 import { injectGlobal } from 'emotion'
-import '../styles/cms.css'
 
 injectGlobal({
 	'.nc-githubAuthenticationPage-logo svg': {
