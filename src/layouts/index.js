790dec92c3bc55694133c1f2e61e55fa18df8342
Kennedy Rose <kennedyianrose@gmail.com>
2018-07-30 10:56:30 -0500

    Removed char set

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,8 @@ import { Helmet } from 'react-helmet'
 import { injectGlobal } from 'emotion'
 import globalStyles from '../styles/global'
 import { title } from '../../site-config'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 injectGlobal(globalStyles)
 
@@ -11,12 +13,13 @@ export default class Layout extends React.Component{
 		return(
 			<Fragment>
 				<Helmet>
-					<meta charSet="utf-8" />
 					<title>{title}</title>
 				</Helmet>
+				<Header />
 				<main>
 					{this.props.children()}
 				</main>
+				<Footer />
 			</Fragment>
 		)
 	}
