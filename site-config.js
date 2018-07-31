aab4f15eaaf302fed5262155e8bddf4f567ed70d
Kennedy Rose <kennedyianrose@gmail.com>
2018-07-31 16:01:16 -0500

    Log site url

--- site-config.js
+++ site-config.js
@@ -1,6 +1,15 @@
+const {
+	CONTEXT,
+	URL,
+	DEPLOY_URL,
+} = process.env
+const siteUrl = CONTEXT === `production` ? URL : DEPLOY_URL
+
+console.log(`siteUrl`, siteUrl)
+
 module.exports = {
 	title: `Gatsby Boilerplate`,
 	shortTitle: `Boilerplate`,
 	description: `A boilerplate for new Gatsby projects.`,
-	siteUrl: `https://www.escaladesports.com`,
+	siteUrl,
 }
