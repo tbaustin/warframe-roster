798732f001e20f39ab5765034d5bb742fe55efb7
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-02 08:39:14 -0500

    Home button

--- src/components/header.js
+++ src/components/header.js
@@ -6,6 +6,7 @@ export default class Header extends React.Component{
 		return (
 			<header>
 				<ul>
+					<li><Link to='/'>Home</Link></li>
 					<li><Link to='/about'>About</Link></li>
 					<li><Link to='/contact'>Contact</Link></li>
 				</ul>
