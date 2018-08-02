a8782b541bf0dd876c50ab74c77245b02f9cf707
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-02 08:59:34 -0500

    CSS template strings

--- src/components/cms.js
+++ src/components/cms.js
@@ -5,26 +5,26 @@ import identity from 'netlify-identity-widget'
 import { injectGlobal } from 'emotion'
 import logo from '../img/logo.png'
 
-injectGlobal({
-	'.nc-githubAuthenticationPage-logo svg': {
-		display: `none !important`,
-	},
-	'.nc-githubAuthenticationPage-logo': {
-		background: `url('${logo}') no-repeat center center !important`,
-		backgroundSize: `contain !important`,
-	},
-	'.nc-githubAuthenticationPage-button': {
-		color: `transparent !important`,
-		'&:after': {
-			content: `"Sign In"`,
-			textAlign: `center`,
-			color: `#fff`,
-			position: `absolute`,
-			left: 0,
-			right: 0,
-		},
-	},
-})
+injectGlobal(`
+	.nc-githubAuthenticationPage-logo svg{
+		display: none !important;
+	}
+	.nc-githubAuthenticationPage-logo{
+		background: url('${logo}') no-repeat center center !important;
+		background-size: contain !important;
+	}
+	.nc-githubAuthenticationPage-button{
+		color: transparent !important;
+		&:after{
+			content: "Sign In";
+			textAlign: center;
+			color: #fff;
+			position: absolute;
+			left: 0;
+			right: 0;
+		}
+	}
+`)
 
 window.netlifyIdentity = identity
 identity.init({
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { css } from 'emotion'
 
 export default class Header extends React.Component{
 	render(){
 		return (
-			<header>
+			<header className={styles}>
 				<ul>
 					<li><Link to='/'>Home</Link></li>
 					<li><Link to='/about'>About</Link></li>
@@ -14,3 +15,19 @@ export default class Header extends React.Component{
 		)
 	}
 }
+
+const styles = css`
+	ul{
+		list-style-type: none;
+		padding: 0;
+		margin: 0;
+	}
+	li{
+		display: inline-block;
+	}
+	a{
+		text-decoration: none;
+		padding: 10px;
+		display: inline-block;
+	}
+`
