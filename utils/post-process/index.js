89df68062d37f507449edf345fca5ba90b2b1f86
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-06 16:52:18 -0500

    Post process CMS

---
+++ utils/post-process/index.js
@@ -0,0 +1,26 @@
+const {
+	readFile,
+	outputFile,
+} = require(`fs-extra`)
+
+const cwd = process.cwd()
+const path = `${cwd}/public/admin/index.html`
+
+async function postProcess(){
+	console.log(`Post processing...`)
+	try{
+		let str = await readFile(path, `utf8`)
+		str = str.replace(
+			`<meta charset="UTF-8">`,
+			`<meta name="googlebot" content="noindex" /><meta charset="UTF-8">`)
+		await outputFile(path, str)
+		console.log(`Post processed`)
+	}
+	catch (err) {
+		console.log(`Failed to post process`)
+		console.error(err)
+		process.exit(1)
+	}
+}
+
+postProcess()
