0e7b43b429237461ab862136ab21d526a684034e
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-06 15:09:46 -0500

    Patch to remove logo before init

--- utils/patch/index.js
+++ utils/patch/index.js
@@ -5,14 +5,16 @@ const {
 
 const cwd = process.cwd()
 const path = `${cwd}/node_modules/netlify-identity-widget/build/netlify-identity-widget.js`
+const find = `logo:t.modal.logo`
+const replace = `logo:false`
 
 async function patch(){
 	try{
 		let str = await readFile(path, `utf8`)
-		str = str.replace(
-			`document.addEventListener("DOMContentLoaded",function(){i.default.init()})`,
-			`document.addEventListener("DOMContentLoaded",function(){})`
-		)
+		if(str.indexOf(find) === -1){
+			throw new Error(`Can't find string to replace`)
+		}
+		str = str.replace(find, replace)
 		await outputFile(path, str)
 		console.log(`Patched identity widget`)
 	}
