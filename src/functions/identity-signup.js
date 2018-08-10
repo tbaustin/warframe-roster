e2ea0b63702e9c9b2f988e7a0c4a6a9d7cd50af9
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-10 16:03:49 -0500

    Removed signup logs

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -3,13 +3,11 @@ const whitelist = [
 ]
 
 export function handler({ body }, _, callback){
-	console.log(`identity-signup`)
 	const { user } = JSON.parse(body)
 	const { email } = user
 	const domain = email.split(`@`)[1]
 	let res = ``
 	let statusCode = 400
-	console.log(domain)
 	if(whitelist.indexOf(domain) !== -1){
 		statusCode = 200
 		res = JSON.stringify({
