ed47fa4ba47d92d619b5da5669b9f3dbfa89c03a
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-10 14:38:33 -0500

    Restrict origin

--- src/functions/empty.js
+++ src/functions/empty.js
@@ -1,6 +1,10 @@
 export function handler(_, __, callback){
 	callback(null, {
 		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': `https://gatsby-boilerplate.netlify.com`,
+			//'Access-Control-Allow-Headers': `Content-Type`,
+		},
 		body: JSON.stringify({
 			success: true,
 		}),
