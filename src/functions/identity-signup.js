a2796cfbee66e118357eef1d1a90b15337224e6f
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-10 16:01:21 -0500

    Log domain

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -3,12 +3,13 @@ const whitelist = [
 ]
 
 export function handler({ body }, _, callback){
-	console.log(`IDENTITY SIGNUP`)
+	console.log(`identity-signup`)
 	const { user } = JSON.parse(body)
 	const { email } = user
 	const domain = email.split(`@`)[1]
 	let res = ``
 	let statusCode = 400
+	console.log(domain)
 	if(whitelist.indexOf(domain) !== -1){
 		statusCode = 200
 		res = JSON.stringify({
