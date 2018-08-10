0fa34c14fc98898bc0020775f5fed1f77d67e14a
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-10 14:34:44 -0500

    Removed headers

--- src/functions/empty.js
+++ src/functions/empty.js
@@ -1,10 +1,6 @@
 export function handler(_, __, callback){
 	callback(null, {
 		statusCode: 200,
-		headers: {
-			'Access-Control-Allow-Origin': `*`,
-			'Access-Control-Allow-Headers': `Content-Type`,
-		},
 		body: JSON.stringify({
 			success: true,
 		}),
