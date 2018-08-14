eef2f93856b7713c0cc44bdb699602d22bfbe827
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-14 08:13:43 -0500

    Separate link style

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,7 @@ export default class Header extends React.Component{
 					<li><Link to='/about'>About</Link></li>
 					<li><Link to='/grid'>Grid</Link></li>
 					<li><Link to='/contact'>Contact</Link></li>
+					<li><a href='/email-templates/invitation'>Email Template</a></li>
 				</ul>
 			</header>
 		)
--- src/pages/email-templates/invitation.js
+++ src/pages/email-templates/invitation.js
@@ -5,22 +5,22 @@ import { Helmet } from 'react-helmet'
 export default class EmailInvitation extends React.Component {
 	render() {
 		return (
-			<div className={wrapperStyles}>
+			<div className={wrapper}>
 				<Helmet>
 					<html lang='en' dir='ltr' />
-					<body bgColor='#fff' width='100%' style={bodyStyles} />
+					<body bgColor='#fff' width='100%' style={body} />
 				</Helmet>
 				<h2>You have been invited</h2>
 				<p>You have been invited to create a user on {`{{ .SiteURL}}`}. Follow this link to accept the invite:</p>
 				<p>
-					<a href='{{ .SiteURL }}/admin/#invite_token={{ .Token }}'>Accept the invite</a>
+					<a className={link} href='{{ .SiteURL }}/admin/#invite_token={{ .Token }}'>Accept the invite</a>
 				</p>
 			</div>
 		)
 	}
 }
 
-const bodyStyles = {
+const body = {
 	textRendering: `optimizeLegibility`,
 	'-webkit-font-smoothing': `antialiased`,
 	'-webkit-text-size-adjust': `none`,
@@ -32,11 +32,12 @@ const bodyStyles = {
 	fontFamily: `-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif`,
 }
 
-const wrapperStyles = css({
+const wrapper = css({
 	maxWidth: 600,
 	padding: 20,
 	margin: `0 auto`,
-	a: {
-		color: `#0f0`,
-	},
+})
+
+const link = css({
+	color: `#0f0`,
 })
