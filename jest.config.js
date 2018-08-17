a6fefce0bd14ad5ddc387103bac4a40921ae3f9d
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-17 12:09:14 -0500

    Ignore directories with jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
 	testEnvironment: `jsdom`,
 	verbose: true,
-	testPathIgnorePatterns: [`<rootDir>/node_modules/`],
+	testPathIgnorePatterns: [
+		`<rootDir>/node_modules/`,
+		`<rootDir>/.cache/`,
+		`<rootDir>/public/`,
+		`<rootDir>/dist/`,
+	],
 	testURL: `http://localhost/`,
 }
