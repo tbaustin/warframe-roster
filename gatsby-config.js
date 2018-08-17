12ee9d9c83b5654281d8daf0669c05bc8b2018aa
Kennedy Rose <kennedyianrose@gmail.com>
2018-08-17 16:25:22 -0500

    Catch markdown links

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
 		},
 		`gatsby-plugin-sharp`,
 		`gatsby-transformer-sharp`,
+		`gatsby-plugin-catch-links`,
 		`gatsby-plugin-remove-trailing-slashes`,
 		`gatsby-plugin-netlify-cms-paths`,
 		{
