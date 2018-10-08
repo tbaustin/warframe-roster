c02ee99a10b1e7363337d54a7830a71b1e493f46
Kennedy Rose <kennedyianrose@gmail.com>
2018-10-08 16:34:22 -0500

    Fuzzier search

--- plugins/search/search.js
+++ plugins/search/search.js
@@ -1,3 +1,4 @@
+import lunr from 'lunr'
 import fetchIndex from './fetch-index'
 import searchState from './state'
 import indexStore from './index-store'
@@ -20,10 +21,29 @@ export default async function search(query){
 
 	const { index, store } = indexStore
 
-	const results = index.search(query)
-		.map(({ ref }) => {
-			return store[ref]
+	query = query.split(` `)
+	let results = index.query(function(q){
+		// Exact matches should have the highest boost
+		q.term(query, { boost: 100 })
+
+		// Prefix matches should be boosted slightly
+		q.term(query, {
+			boost: 10,
+			usePipeline: false,
+			wildcard: lunr.Query.wildcard.TRAILING,
 		})
+
+		// Finally, try a fuzzy search, without any boost
+		q.term(query, {
+			boost: 1,
+			usePipeline: false,
+			editDistance: 3,
+		})
+	})
+
+	results = results.map(({ ref }) => {
+		return store[ref]
+	})
 
 	if(curId !== id) return
 
