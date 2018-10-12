8a3a003cec453c9f1ec44d2b6955f7cf11dba280
Kennedy Rose <kennedyianrose@gmail.com>
2018-10-12 13:37:55 -0500

    Removed search plugin state

--- plugins/search/search.js
+++ plugins/search/search.js
@@ -1,21 +1,11 @@
 import lunr from 'lunr'
 import fetchIndex from './fetch-index'
-import searchState from './state'
 import indexStore from './index-store'
 
-let id = 0
-
 export default async function search(query){
-	searchState.setState({ query })
 	if (!query) {
-		searchState.setState({ results: [] })
-		return
+		return []
 	}
-
-	id++
-	const curId = id
-
-	searchState.setState({ loading: true })
 
 	await fetchIndex()
 
@@ -57,10 +47,5 @@ export default async function search(query){
 		return store[ref]
 	})
 
-	if(curId !== id) return
-
-	searchState.setState({
-		loading: false,
-		results,
-	})
+	return results
 }
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,48 +1,66 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { graphql } from 'gatsby'
 import Link from 'gatsby-link'
 import Layout from '../components/layouts/default'
-import { search, SearchResults } from '../../plugins/search'
+import { search } from '../../plugins/search'
 
 export default class SearchPage extends React.Component{
+	constructor(props){
+		super(props)
+		this.id = 0
+		this.state = {
+			loading: false,
+			results: [],
+		}
+	}
+	async search(term){
+		this.id++
+		const curId = this.id
+		this.setState({ loading: true })
+		const results = await search(term)
+		if (curId === this.id) {
+			this.setState({
+				loading: false,
+				results,
+			})
+		}
+	}
 	render(){
 		const {
 			siteTitle,
 			siteDescription,
 		} = this.props.data.site.siteMetadata
+		const {
+			loading,
+			results,
+		} = this.state
 		return(
 			<Layout title='Search' siteTitle={siteTitle} description={siteDescription}>
 				<h1>Search</h1>
 				<input
 					type='text'
-					onChange={e => search(e.target.value)}
+					onChange={e => this.search(e.target.value)}
 				/>
-				<SearchResults>
-					{({ results, hasResults, loading }) => (
-						<Fragment>
-							{loading && (
-								<div>Loading...</div>
-							)}
-							{!hasResults && !loading && (
-								<div>No results found.</div>
-							)}
-							{hasResults && (
-								<ul>
-									{results.map(({ title, excerpt, path }, index) => (
-										<li key={`search-result-${index}`}>
-											<div>
-												<Link to={path}>{title}</Link>
-											</div>
-											<div>
-												{excerpt} <Link to={path}>Read more</Link>
-											</div>
-										</li>
-									))}
-								</ul>
-							)}
-						</Fragment>
-					)}
-				</SearchResults>
+				{loading && (
+					<div>Loading...</div>
+				)}
+				{!results.length && !loading && (
+					<div>No results found.</div>
+				)}
+				{!!results.length && (
+					<ul>
+						{results.map(({ title, excerpt, path }, index) => (
+							<li key={`searchResult${index}`}>
+								<div>
+									<Link to={path}>{title}</Link>
+								</div>
+								<div>
+									{excerpt} <Link to={path}>Read more</Link>
+								</div>
+							</li>
+						))}
+					</ul>
+				)}
 			</Layout>
 		)
 	}
