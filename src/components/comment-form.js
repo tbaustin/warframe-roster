b33c65af70b65bb187d49440b44f4a2d1de9c742
Kennedy Rose <kennedyianrose@gmail.com>
2018-11-27 14:18:56 -0600

    Comment to contentful lambda

--- src/components/comment-form.js
+++ src/components/comment-form.js
@@ -16,7 +16,7 @@ export default class CommentForm extends React.Component{
 	render(){
 		return (
 			<Form
-				action='/.netlify/functions/comment-to-markdown'
+				action='/.netlify/functions/comment-to-contentful'
 				initialValues={{
 					email: ``,
 					name: ``,
