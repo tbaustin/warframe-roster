f41a021cf453b2168222d90cfae9f0c18c22625e
Kennedy Rose <kennedyianrose@gmail.com>
2018-12-11 09:47:49 -0600

    Removed redirects comments

--- plugins/contentful-redirects/gatsby-node.js
+++ plugins/contentful-redirects/gatsby-node.js
@@ -1,9 +1,7 @@
 const redirects = require(`.cache/contentful-redirects.json`)
 
 exports.createPages = ({ actions: { createRedirect } }) => {
-	console.log(`REDIRECTS`, redirects)
 	redirects.map(data => {
-		console.log(`\n`, `REDIRECT:`, data)
 		return createRedirect(data)
 	})
 }
