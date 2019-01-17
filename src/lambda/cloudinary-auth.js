93d47a7a20b1e792830c590ff2c7c41384d6a958
Kennedy Rose <kennedyianrose@gmail.com>
2019-01-17 11:18:17 -0600

    Log context

--- src/lambda/cloudinary-auth.js
+++ src/lambda/cloudinary-auth.js
@@ -1,10 +1,16 @@
-import { parse } from 'cookie'
 
-export function handler(req, _, callback){
 
-	const { nf_jwt } = parse(req.headers.cookie)
-	console.log(`jwt`, nf_jwt)
-	console.log(process.env)
+export function handler(_, context, callback){
+
+	// const { nf_jwt } = parse(req.headers.cookie)
+	// console.log(`jwt`, nf_jwt)
+
+	// const auth = new GoTrue({
+	// 	APIUrl: `${process.env.URL}/.netlify/identity`,
+	// })
+
+	// auth
+	console.log(context)
 
 	callback(null, {
 		statusCode: 200,
