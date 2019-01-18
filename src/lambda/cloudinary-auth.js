0759f4efe4b104f1628f53e9b91dcea46a2be9b5
Kennedy Rose <kennedyianrose@gmail.com>
2019-01-18 14:19:51 -0600

    Log verification attempts

--- src/lambda/cloudinary-auth.js
+++ src/lambda/cloudinary-auth.js
@@ -1,9 +1,17 @@
+import jwt from 'jsonwebtoken'
 
+const { JWT_SIGNING_SECRET } = process.env
 
 export function handler(body, context, callback){
 
-	console.log(`BODY`, body)
-	console.log(`CONTEXT`, context)
+	// console.log(`BODY`, body)
+	// console.log(`CONTEXT`, context)
+
+	const bodyRes = jwt.verify(body.headers.cookie.split(`nf_jwt=`)[1], JWT_SIGNING_SECRET)
+	console.log(`bodyRes`, bodyRes)
+
+	const contextRes = jwt.verify(context.clientContext.identity.token, JWT_SIGNING_SECRET)
+	console.log(`contextRes`, contextRes)
 
 	callback(null, {
 		statusCode: 200,
