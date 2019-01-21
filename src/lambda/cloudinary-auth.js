075d2d3c4de7279efaf2915defceef983ea04794
Kennedy Rose <kennedyianrose@gmail.com>
2019-01-21 09:22:36 -0600

    Send signature

--- src/lambda/cloudinary-auth.js
+++ src/lambda/cloudinary-auth.js
@@ -1,7 +1,13 @@
 import jwt from 'jsonwebtoken'
 import { parse } from 'cookie'
+import { createHash } from 'crypto'
 
-const { JWT_SIGNING_SECRET } = process.env
+const {
+	JWT_SIGNING_SECRET,
+	CLOUDINARY_CLOUD_NAME,
+	CLOUDINARY_USERNAME,
+	CLOUDINARY_API_SECRET,
+} = process.env
 
 export function handler(body, context, callback){
 
@@ -16,10 +22,14 @@ export function handler(body, context, callback){
 		if(roles.indexOf(`admin`) === -1){
 			throw `Admin role not found`
 		}
+		const ts = Math.round((new Date()).getTime() / 1000)
+		const sigString = `cloud_name=${CLOUDINARY_CLOUD_NAME}&timestamp=${ts}&username=${CLOUDINARY_USERNAME}${CLOUDINARY_API_SECRET}`
+		const signature = createHash(`sha256`).update(sigString).digest(`hex`)
+		console.log(`signature`, signature)
 		callback(null, {
 			statusCode: 200,
 			body: JSON.stringify({
-				success: true,
+				signature,
 			}),
 		})
 	}
