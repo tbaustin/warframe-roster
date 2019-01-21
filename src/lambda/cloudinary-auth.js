9964851eccd5ec4f57565a1b2715f5934ea76c2a
Kennedy Rose <kennedyianrose@gmail.com>
2019-01-21 09:04:51 -0600

    Check for admin role

--- src/lambda/cloudinary-auth.js
+++ src/lambda/cloudinary-auth.js
@@ -5,45 +5,33 @@ const { JWT_SIGNING_SECRET } = process.env
 
 export function handler(body, context, callback){
 
-	// console.log(`BODY`, body)
-	// console.log(`CONTEXT`, context)
-
 	const { nf_jwt } = parse(body.headers.cookie)
 
-	console.log(`JWT`, nf_jwt)
-	console.log(`Signing secret`, JWT_SIGNING_SECRET)
-
-	console.log(
-		`bodyRes with secret`,
-		jwt.verify(nf_jwt, JWT_SIGNING_SECRET)
-	)
-
-	// console.log(
-	// 	`contextRes with secret`,
-	// 	jwt.verify(context.clientContext.identity.token, JWT_SIGNING_SECRET)
-	// )
-
-
-	try {
-		console.log(
-			`bodyRes with bunk`,
-			jwt.verify(nf_jwt, `-xN_h82PHVTCMA9vdoHrcZxH-x5mb11y1537t3rGzcM`)
-		)
+	try{
+		const {
+			app_metadata: {
+				roles,
+			},
+		} = jwt.verify(nf_jwt, JWT_SIGNING_SECRET)
+		if(roles.indexOf(`admin`) === -1){
+			throw `Admin role not found`
+		}
+		callback(null, {
+			statusCode: 200,
+			body: JSON.stringify({
+				success: true,
+			}),
+		})
 	}
 	catch(err){
 		console.error(err)
+		callback(null, {
+			statusCode: 403,
+			body: JSON.stringify({
+				success: false,
+			}),
+		})
 	}
 
 
-	// console.log(
-	// 	`contextRes with bunk`,
-	// 	jwt.verify(context.clientContext.identity.token, `asdf`)
-	// )
-
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({
-			success: true,
-		}),
-	})
 }
