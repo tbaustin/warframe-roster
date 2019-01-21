2bf9f286225e9531320c12a2882a139f43314318
Kennedy Rose <kennedyianrose@gmail.com>
2019-01-21 08:43:53 -0600

    Cookie parsing

--- src/lambda/cloudinary-auth.js
+++ src/lambda/cloudinary-auth.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import { parse } from 'cookie'
 
 const { JWT_SIGNING_SECRET } = process.env
 
@@ -7,25 +8,42 @@ export function handler(body, context, callback){
 	// console.log(`BODY`, body)
 	// console.log(`CONTEXT`, context)
 
+	const { nf_jwt } = parse(body.headers.cookie)
+
 	console.log(
 		`bodyRes with secret`,
-		jwt.verify(body.headers.cookie.split(`nf_jwt=`)[1], JWT_SIGNING_SECRET)
+		jwt.verify(nf_jwt, JWT_SIGNING_SECRET)
 	)
 
-	console.log(
-		`contextRes with secret`,
-		jwt.verify(context.clientContext.identity.token, JWT_SIGNING_SECRET)
-	)
+	// console.log(
+	// 	`contextRes with secret`,
+	// 	jwt.verify(context.clientContext.identity.token, JWT_SIGNING_SECRET)
+	// )
 
 
-	console.log(
-		`bodyRes with bunk`,
-		jwt.verify(body.headers.cookie.split(`nf_jwt=`)[1], `asdf`)
-	)
-	console.log(
-		`contextRes with bunk`,
-		jwt.verify(context.clientContext.identity.token, `asdf`)
-	)
+	try {
+		console.log(
+			`bodyRes with bunk`,
+			jwt.verify(nf_jwt, `asdf`)
+		)
+	}
+	catch(err){
+		console.error(err)
+	}
+
+	try {
+		console.log(
+			`bodyRes with nothing`,
+			jwt.verify(nf_jwt)
+		)
+	}
+	catch (err) {
+		console.error(err)
+	}
+	// console.log(
+	// 	`contextRes with bunk`,
+	// 	jwt.verify(context.clientContext.identity.token, `asdf`)
+	// )
 
 	callback(null, {
 		statusCode: 200,
